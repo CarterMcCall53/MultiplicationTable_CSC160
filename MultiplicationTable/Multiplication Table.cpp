90434cf3cd81bebeb2e8cb6457d34ab5bf578c8c
Carter McCall <Carter_McCall53@outlook.com>
2022-09-15 09:44:07 -0600

    Made multiplication table editable

--- MultiplicationTable/Multiplication Table.cpp
+++ MultiplicationTable/Multiplication Table.cpp
@@ -10,10 +10,14 @@
 using namespace std;
 
 int main() {
-	cout << "12 x 12 Multiplication Table:\n";
-	const int MAX = 12;
-	for (int i = 0; i <= MAX; i++) {
-		for (int j = 0; j <= MAX; j++) {
+	int max;
+	do {
+		cout << "What would you like the max number to be on the multiplication table? (Max of 30): ";
+		cin >> max;
+	} while (max > 30);
+	cout << max << " by " << max << " Multiplication Table:" << endl;
+	for (int i = 1; i <= max; i++) {
+		for (int j = 1; j <= max; j++) {
 			cout << setw(4) << i * j;
 		}
 		cout << "\n";
