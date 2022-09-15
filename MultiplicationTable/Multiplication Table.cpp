980c437ce44f32dd852d2bd2777e840f4cbffca8
Carter McCall <Carter_McCall53@outlook.com>
2022-09-15 09:30:35 -0600

    Add project files.

---
+++ MultiplicationTable/Multiplication Table.cpp
@@ -0,0 +1,21 @@
+/* Multiplication Table
+*  Carter McCall
+*  15 September 2022
+*  Purpose:
+*		Create a multiplication table
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout << "12 x 12 Multiplication Table:" << endl;
+	const int MAX = 12;
+	for (int i = 0; i <= MAX; i++) {
+		for (int j = 0; j <= MAX; j++) {
+			cout << setw(4) << i * j;
+		}
+		cout << "\n";
+	}
+}
