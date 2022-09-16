03492fc8525355fab390430b5d32bc6b74929baa
Carter McCall <Carter_McCall53@outlook.com>
2022-09-16 09:17:46 -0600

    Fixed bugged code

--- MultiplicationTable/Multiplication Table.cpp
+++ MultiplicationTable/Multiplication Table.cpp
@@ -3,6 +3,8 @@
 *  15 September 2022
 *  Purpose:
 *		Create a multiplication table
+*  Modified 16 September 2022:
+*		Added extra do-while loop at line 19 because there were errors
 */
 
 #include <iostream>
@@ -10,16 +12,25 @@
 using namespace std;
 
 int main() {
-	int max;
+	string input;
+	char repChar;
+	int max = 0;
 	do {
-		cout << "What would you like the max number to be on the multiplication table? (6-22): ";
-		cin >> max; 
-	} while (max > 22 && max < 6);
-	cout << max << " by " << max << " Multiplication Table:" << endl;
-	for (int i = 1; i <= max; i++) {
-		for (int j = 1; j <= max; j++) {
-			cout << setw(4) << i * j;
+		do {
+			do {
+				cout << "What would you like the max number to be on the multiplication table? (6-22): ";
+				cin >> max;
+			} while (max < 6);
+		} while (max > 22);
+		cout << max << " by " << max << " Multiplication Table:" << endl;
+		for (int i = 1; i <= max; i++) {
+			for (int j = 1; j <= max; j++) {
+				cout << setw(4) << i * j;
+			}
+			cout << "\n";
 		}
-		cout << "\n";
-	}
+		cout << "\nDo you want to see another table? (Y|N): ";
+		cin >> input;
+		repChar = tolower(input[0]);
+	} while (repChar == 'y' && repChar != 'n');
 }
